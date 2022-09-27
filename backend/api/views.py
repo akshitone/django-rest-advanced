1615722e1eea9585bd1b83c9cc04a861deab80d4
akshitone <akshitone8@gmail.com>
2022-09-27 15:03:28 +0530

    implemented api_view decorator to home api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,21 @@
-import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from product.models import Product
 
 
+@api_view(['GET'])
 def home(request):
+    """
+    DRF API View
+    """
     product = Product.objects.all().order_by('?').first()
     output_product = dict()
 
     # model instance -> python dict
     if product:
-        output_product = model_to_dict(product)
+        output_product = model_to_dict(product, fields=['title', 'price'])
 
-    return JsonResponse(output_product)
+    return Response(output_product)
